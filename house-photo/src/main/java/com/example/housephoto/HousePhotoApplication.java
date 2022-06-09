6a3be5873319348526a080fb5be29d82223a4633
whl520 <3044797391@qq.com>
2022-06-09 17:32:51 +0800

    Initial commit

---
+++ house-photo/src/main/java/com/example/housephoto/HousePhotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.housephoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousePhotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousePhotoApplication.class, args);
+    }
+
+}
---
+++ house-photo/src/test/java/com/example/housephoto/HousePhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housephoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousePhotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
